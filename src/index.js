e637a48389b828d28ce41a9cce49cce5854a9b8b
flexcommits <andersensven.star@gmail.com>
2020-07-11 12:20:59 -0400

    edited Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 
